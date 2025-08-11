f8744f856159698a49e4fc74e71c820de569b58b
sisu <obilaorchisomebi123@gmail.com>
2025-08-12 00:07:18 +0100

    refactor:Onboarding screen

--- lib/view/screens/dashboard/onboarding_screen.dart
+++ lib/view/screens/dashboard/onboarding_screen.dart
@@ -19,35 +19,46 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       backgroundColor: SpentColors.kMintWhisper,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 16),
           child: Column(
             children: [
-              TextView(
-                text: SpentStrings.trackYourSpendingEffortlessly,
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
+              Spacer(),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    TextView(
+                      text: SpentStrings.trackYourSpendingEffortlessly,
+                      fontWeight: FontWeight.bold,
+                      textAlign: TextAlign.center,
+                      fontSize: 22,
+                      maxLines: 3,
+                    ),
+                    Gap(16),
+                    TweenAnimationBuilder(
+                      tween: Tween(begin: 0.8, end: 1.0),
+                      duration: Duration(milliseconds: 350),
+                      curve: Curves.easeOutBack,
+                      builder: (context, value, child) {
+                        return Transform.scale(
+                          scale: value,
+                          child: ImageView.asset(SpentImages.noteDollarIcon, scale: 2.0),
+                        );
+                      },
+                    ),
+                    Gap(16),
+                    TextView(text: SpentStrings.balanceSheetDescription, fontSize: 16, maxLines: 3),
+                    Gap(24),
+                    SpentButtonMain(
+                      text: SpentStrings.getStarted,
+                      borderRadius: 8,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
               ),
-              Gap(8),
-              TweenAnimationBuilder(
-                tween: Tween(begin: 0.8, end: 1.0),
-                duration: Duration(milliseconds: 350),
-                curve: Curves.easeOutBack,
-                builder: (context, value, child) {
-                  return Transform.scale(
-                    scale: value,
-                    child: ImageView.asset(SpentImages.noteDollarIcon, scale: 2.0),
-                  );
-                },
-              ),
-              Gap(8),
-              TextView(text: SpentStrings.balanceSheetDescription, fontSize: 16),
-              Gap(16),
-              SpentButtonMain(
-                text: SpentStrings.getStarted,
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                onPressed: () {},
-              )
             ],
           ),
         ),

c1b128432f9e9f536583e06e01de4eb07f145dd2
sisu <obilaorchisomebi123@gmail.com>
2025-08-16 12:10:04 +0100

    refactor: AppBar widget height spacing

--- lib/view/components/app_bar.dart
+++ lib/view/components/app_bar.dart
@@ -6,20 +6,23 @@ class SpentAppBar {
   ///Create a widget displaying a back icon and a text title underneath.
   static PreferredSizeWidget mainAppBar(BuildContext context,{required String title}) {
     return PreferredSize(
-      preferredSize: Size(0, 90),
+      preferredSize: Size(0, 130),
       child: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Icon(Icons.arrow_back, color: SpentColors.kTextBlack, size: 25),
-            ),
-            Gap(8),
-            TextView(text: title, fontSize: 28, fontWeight: FontWeight.bold)
-          ],
+        child: Padding(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.arrow_back, color: SpentColors.kTextBlack, size: 25),
+              ),
+              Gap(8),
+              TextView(text: title, fontSize: 28, fontWeight: FontWeight.bold, maxLines: 2),
+            ],
+          ),
         ),
       ),
     );
